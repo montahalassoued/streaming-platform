316a6764d17aab5a7bd1a315a6de8b5b8ebdf8f6
raed-saidi <raed.saidi@insat.ucar.tn>
2026-05-27 20:38:15 +0100

    Enable rawBody for webhook verification

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { rawBody: true });
   app.enableCors({
     origin: process.env.FRONTEND_URL ?? "http://localhost:8083",
     credentials: true,
