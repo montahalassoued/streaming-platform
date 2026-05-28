8ab031450ece3c156ef2484317e9d7687527ddc6
montahalassoued <montahalassoued8@gmail.com>
2026-05-28 11:25:07 +0100

    chore: repo cleanup and live/renewal basics
    Remove build artifacts, add Redis live events and monthly subscription renewal.

--- dist_manual/migrations/1779717949377-InitMigration.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.InitMigration1779717949377 = void 0;
-class InitMigration1779717949377 {
-    async up(queryRunner) { }
-    async down(queryRunner) { }
-}
-exports.InitMigration1779717949377 = InitMigration1779717949377;
